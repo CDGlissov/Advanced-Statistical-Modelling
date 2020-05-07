02495c3f35eb1508e54f72764eacd6adc2ddeca4
Christian D. Glissov <chris.glissov@gmail.com>
2020-05-07 15:03:25 +0200

    updated

--- Assignment 3/assignment3_chris.R
+++ Assignment 3/assignment3_chris.R
@@ -17,6 +17,7 @@ library(ggpubr);
 library(ggfortify);
 library(GGally);
 library(reshape2)
+library(DHARMa)
 ggplot2::theme_set(ggplot2::theme_grey())
 gg_color_hue <- function(n) {
   hues = seq(15, 375, length = n + 1)
@@ -158,63 +159,185 @@ ev <- eigen(Sigma0.m)
 ## Problem A
 
 clo <- read.csv("dat_count3.csv", sep=";")
-clo$subjId = as.factor(clo$subjId)
+#clo$subjId = as.factor(clo$subjId)
 clo$day=as.factor(clo$day)
 clo$sex=as.factor(clo$sex)
 summary(clo)
 str(clo)
 
+################## Binomial#################
+#let's try with a simple model first
+fit.glm.bin1=glmmTMB(cbind(clo,nobs-clo)~time+day+sex+tOut+tInOp+(1|subjId), 
+                    family=binomial(link = "logit"), data=clo, REML=FALSE)
 
-fit.glm.bin1=glmmTMB(cbind(clo,nobs-clo)~time+sex+tOut+tInOp+(1|subjId), 
-                    family=binomial(link = "logit"), data=clo)
+# Reducing
+Anova(fit.glm.bin1, type="II")
+fit.glm.bin2<-update(fit.glm.bin1,~.-tOut)
+Anova(fit.glm.bin2, type="II")
+fit.glm.bin3<-update(fit.glm.bin2,~.-time)
+Anova(fit.glm.bin3, type="II")
+fit.glm.bin4<-update(fit.glm.bin3,~.-tInOp)
+Anova(fit.glm.bin4, type="II")
+fit.glm.bin5<-update(fit.glm.bin4,~.-day)
+Anova(fit.glm.bin5, type="II")
 
-fit.glm.bin2=glmmTMB(cbind(clo,nobs-clo)~time+sex+tOut+tInOp+ar1(as.factor(day)-1|subjId), 
-        family=binomial(link = "logit"), data=clo)
-fit.glm.bin1
-fit.glm.bin2
 
-fit.glm.bin2=glmmTMB(cbind(clo,nobs-clo)~time+sex+tOut+tInOp+ar1(as.factor(day)-1|subjId), 
-                     family=binomial(link = "logit"), data=clo)
+#FINAL MODEL
+fit.glm.bin.ml=glmmTMB(cbind(clo,nobs-clo)~sex+(1|subjId), 
+                       family=binomial(link = "logit"), data=clo, REML=FALSE)
+fit.glm.bin=glmmTMB(cbind(clo,nobs-clo)~sex+(1|subjId), 
+                     family=binomial(link = "logit"), data=clo, REML=TRUE)
 
-#t?nk over offset(log(time))
-fit.glm.pois<-glmmTMB(clo~time+nobs+sex+tOut+tInOp,family=poisson(link="log"),
-                 data=clo)
-predict(fit.glm.pois)
-#lav plots
 
-### Part B ###
+
+## POISSON, https://stats.stackexchange.com/questions/11182/when-to-use-an-offset-in-a-poisson-regression
+
+
+fit.glm.pois1<-glmmTMB(clo~day+sex+tOut+tInOp+(1|subjId),offset = log(time),family=poisson(link="log"),
+                      data=clo, REML=FALSE)
+
+
+Anova(fit.glm.pois1, type="II")
+fit.glm.pois2<-update(fit.glm.pois1,~.-nobs)
+Anova(fit.glm.pois2, type="II")
+fit.glm.pois3<-update(fit.glm.pois2,~.-tOut)
+Anova(fit.glm.pois3, type="II")
+fit.glm.pois4<-update(fit.glm.pois3,~.-tInOp)
+Anova(fit.glm.pois4, type="II")
+fit.glm.pois5<-update(fit.glm.pois4,~.-day)
+
+summary(fit.glm.pois5)
+
+fit.glm.pois.ml<-glmmTMB(clo~sex+(1|subjId), offset=log(time), family=poisson(link="log"),data=clo, REML=FALSE)
+fit.glm.pois<-glmmTMB(clo~sex+(1|subjId), offset=log(time), family=poisson(link="log"),data=clo, REML=TRUE)
+
+###COMPARE
+summary(fit.glm.pois.ml)
+summary(fit.glm.bin.ml)
+
+#reml estimates
+c(exp(-2.1378), exp(-1.0834))
+c(exp(-3.588)/(1+exp(-3.588)), exp(-1.235)/(1+exp(-1.235)))
+
+#goodness of fit
+(1-pchisq(259.5,133)) #poisson
+(1-pchisq(257.4,133)) #binomial
+
+
+##### DIAGNOSTICS
+#random effect
+ran.ef.pois = exp(as.vector(unlist(ranef(fit.glm.pois))))
+data1 = data.frame(x=1:47, y=ran.ef.pois)
+ran.ef.bin = exp(as.vector(unlist(ranef(fit.glm.bin))))
+data2 = data.frame(x=1:47, y=ran.ef.bin)
+
+
+#plot random effects
+plot1=ggplot(data1, aes(x=factor(x), y=ran.ef.pois)) + geom_point() + xlab("subject ID") +
+  ylab("Random effect") +ylim(c(0,4.2))+ geom_linerange(aes(x=x, ymax=ran.ef.pois, ymin=0))+
+  geom_text(aes(label=round(ran.ef.pois,2)),hjust=-0.2, vjust=0.3,angle = 90)+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5))
+
+plot2=ggplot(data2, aes(x=factor(x), y=ran.ef.bin)) + geom_point() + xlab("subject ID") +
+  ylab("Random effect") +ylim(c(0,4.2))+ geom_linerange(aes(x=x, ymax=ran.ef.bin, ymin=0))+
+  geom_text(aes(label=round(ran.ef.bin,2)),hjust=-0.2, vjust=0.3,angle = 90)+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5))
+grid.arrange(plot1, plot2, ncol=2)
+
+#qqplots
+par(mfrow=c(2,2))
+qqnorm(ran.ef.pois, col = "blue",main="Poisson: Random Effects")
+qqline(ran.ef.pois, col= "blue")
+qqnorm(ran.ef.bin, col = "blue",main="Binomial: Random Effects")
+qqline(ran.ef.bin, col= "blue")
+qqnorm(resid(fit.glm.pois), col =gg_color_hue(2), main="Poisson: Fixed Effects")
+qqline(resid(fit.glm.pois), col="blue")
+qqnorm(resid(fit.glm.bin), col =gg_color_hue(2), main="Binomial: Fixed Effects")
+qqline(resid(fit.glm.bin), col="blue")
+
+#residuals
+par(mfrow=c(1,2))
+plot(fitted(fit.glm.pois),residuals(fit.glm.pois, "pearson") , main="Poisson: Residuals against fitted",
+     col=gg_color_hue(2), ylab="Pearson Residual", xlab="Fitted")
+abline(0,0)
+plot(fitted(fit.glm.bin),residuals(fit.glm.bin, "pearson") , main="Binomial: Residuals against fitted",
+     col=gg_color_hue(2), ylab="Pearson Residual", xlab="Fitted")
+abline(0,0)
+
+##### PREDICTING POPULATION #POISSON
+maxT<-max(clo$time)
+minT<-min(clo$time)
+newdata<-data.frame(time=rep(seq(minT,maxT,length.out=68),2), subjId=NA)
+nPois<-length(newdata$time)
+#half male and half female
+newdata$sex<-c(rep("female",nPois/2),rep("male",nPois/2))
+predPois <- predict(fit.glm.pois,newdata,se.fit=TRUE)
+
+newdata$Pred<-exp(predPois$fit)
+newdata$UpperConf<-exp(predPois$fit + 1.96 *predPois$se.fit)
+newdata$LowerConf<-exp(predPois$fit - 1.96 *predPois$se.fit)
+
+# Plotting
+p1<-ggplot(newdata)+geom_ribbon(aes(x = time,ymin = LowerConf,ymax=UpperConf,color = sex),alpha = 0.2)+
+  geom_line(aes(x= time,y=Pred,color = sex))+ geom_point(data=clo,aes(x=time,y=clo,colour= sex))
+p1
+
+##### PREDICTING POPULATION #BINOMIAL
+maxT<-max(clo$time)
+minT<-min(clo$time)
+newdata<-data.frame(time=rep(seq(minT,maxT,length.out=68),2), subjId=NA)
+nPois<-length(newdata$time)
+#half male and half female
+newdata$sex<-c(rep("female",nPois/2),rep("male",nPois/2))
+predPois <- predict(fit.glm.bin,newdata,se.fit=TRUE)
+
+invlogit = function(x){
+  return(exp(x)/(1+exp(x)))
+}
+
+newdata$Pred<-invlogit(predPois$fit)
+newdata$UpperConf<-invlogit(predPois$fit + 1.96 *predPois$se.fit)
+newdata$LowerConf<-invlogit(predPois$fit - 1.96 *predPois$se.fit)
+
+# Plotting
+p2<-ggplot(newdata)+geom_ribbon(aes(x = time,ymin = LowerConf,ymax=UpperConf,color = sex),alpha = 0.2)+
+  geom_line(aes(x= time,y=Pred,color = sex))+ geom_point(data=clo,aes(x=time,y=clo,colour= sex))
+p2
+
+##### PREDICTING BASED ON SUBJECTS NEED TO MAKE FOR MORE SUBJECTS AND BINOMIAL/POISSON
+maxT<-max(clo$time)
+minT<-min(clo$time)
+
+newdata<-data.frame(time=rep(seq(minT,maxT,length.out=68),2), subjId=5)
+nPois<-length(newdata$time)
+newdata$sex<-c(rep("female",nPois/2),rep("male",nPois/2))
+predPois <- predict(fit.glm.pois,newdata,se.fit=TRUE, allow.new.levels=FALSE)
+newdata$Pred<-exp(predPois$fit)
+newdata$UpperConf<-exp(predPois$fit + 1.96 *predPois$se.fit)
+newdata$LowerConf<-exp(predPois$fit - 1.96 *predPois$se.fit)
+
+# Plotting
+p<-ggplot(newdata)+geom_ribbon(aes(x = time,ymin = LowerConf,ymax=UpperConf,color = sex),alpha = 0.2)+
+  geom_line(aes(x= time,y=Pred,color = sex))+ geom_point(data=clo,aes(x=time,y=clo,colour= sex))
+p
+
+
+
+###### Part B #######
 library(numDeriv)
 
-X <- matrix(0,ncol=6,nrow=dim(clo)[1])
+X <- matrix(0,ncol=2,nrow=dim(clo)[1])
 X[,1] = 1
-X[,2] = clo$time
-X[,3] = clo$nobs
-X[,4] = clo$sex
-X[,5] = clo$tOut
-X[,6] = clo$tInOp
+X[,2][clo$sex=="male"] = 1 
 
+# Joint probability
 nll <- function(u,beta,sigma.u,X){
-  # One u for every subject that are the same for each
+  # One u for every observation, i.e. will have more of the same
   U <- u[clo$subjId]
-  mu <- exp(X%*%beta)
-  -sum(dpois(clo$clo,lambda=mu*U,log=TRUE)
-         + dgamma(u, shape=sigma.u, scale=1/sigma.u, log=TRUE))
+  eta <- X%*%beta+log(clo$time)
+  -sum(dpois(clo$clo,lambda=exp((eta + log(U))),log=TRUE)) -
+         sum(dgamma(u, shape=sigma.u, scale=1/sigma.u,log=TRUE))
 }
-
-nll.LA <- function(theta,X){
-  beta <- theta[1:dim(X)[2]]
-  sigma.u <- exp(theta[dim(X)[2]+1])
-  est <- nlminb(exp(rep(0,47)), objective = nll,
-                beta=beta, sigma.u=sigma.u,X=X,lower = rep(0.0000001,47))
-  u <- est$par
-  l.u <- est$objective
-  H <- hessian(func = nll, x = u, beta = beta, sigma.u = sigma.u,
-               X=X)
-  l.u + 0.5 * log(det(H/(2*pi)))
-}
-
-system.time((fit1 <- nlminb(c(rep(0,6),1),nll.LA,X=X)))
-fit1 
 
 nll.LA2 <- function(theta,X){
   beta <- theta[1:dim(X)[2]]
@@ -224,31 +347,34 @@ nll.LA2 <- function(theta,X){
     u[i]<-ui
     nll(u,beta,sigma.u,X)
   }
-  u <- numeric(47)
+  u <- rep(1,47)
   ## Use grouping structure
   for(i in 1:length(u)){
     u[i] <- nlminb(1,objective = fun.tmp, u=u,beta=beta,
-                   sigma.u=sigma.u,X=X,i=i, lower=rep(0,47))$par
+                   sigma.u=sigma.u,X=X,i=i, lower=rep(0.01,47))$par
   }
   l.u <- nll(u,beta,sigma.u,X)
   H <- numeric(length(u))
   for(i in 1:length(u)){
-    H[i] <- hessian(func = fun.tmp, x = u[i],u=u, beta = beta, 
+    H[i] <- hessian(func = fun.tmp, x = u[i], u=u, beta = beta, 
                     sigma.u = sigma.u, X=X,i=i)}
   l.u + 0.5 * log(prod(H/(2*pi)))
 }
 
-system.time((fit2 <- nlminb(c(rep(0,6),1),nll.LA2,X=X)))
-fit2
+system.time((fit1 <- nlminb(c(-2,-1,1),nll.LA2,X=X)))
+fit1
+
+opt <- nlminb(rep(1,47),objective = nll, beta = fit1$par[1:2], sigma.u = fit1$par[3], 
+              X = X, lower = rep(0,47))
+hist(opt$par)
 
 ## Check accuracy by importance sampling
-
-nll.LA4Rexsamp <- function(theta,X,k,seed){
+nll.LA.ImportWeight <- function(theta,X,k,seed){
   set.seed(seed)
   beta <- theta[1:dim(X)[2]]
   sigma.u <- exp(theta[dim(X)[2]+1])
   est <- nlminb(rep(1,47),objective = nll,
-                beta=beta, sigma.u=sigma.u,X=X, lower=0)
+                beta=beta, sigma.u=sigma.u,X=X, lower=rep(0.001,47))
   u <- est$par
   l.u <- est$objective
   H <- diag(hessian(func = nll, x = u, beta = beta, sigma.u = sigma.u,
@@ -258,41 +384,20 @@ nll.LA4Rexsamp <- function(theta,X,k,seed){
   for(i in 1:k){## Simulation part
     u.sim <- rgamma(length(u), shape=sigma.u, scale=1/sigma.u)
     L[i] <- exp(-nll(u.sim,beta,sigma.u,X))/
-      prod(dnorm(u.sim,mean=u,sd=s))
+      prod(dgamma(u.sim, shape=sigma.u, scale=1/sigma.u))
   }
   -log(mean(L))
 }
 
-k <- 100000
-L <- nll.LA4Rexsamp(fit1$par,X,k=k,seed=16345)
-c(L,fit1$objective, fit2$objective)
-
-#Plotting the confidence. This can take a long time
-n<-1000
-L.sim <- numeric(n)
-for(i in 1:n){
-  L.sim[i] <- exp(-nll.LA4Rexsamp(fit1$par,X,k=1,seed=i))
-}
-
-## For plotting
-cs <- cumsum(L.sim)/(1:n)
-css <- cumsum(L.sim^2)/1:n
-csse <- css/(1:n)-(cs/1:n)^2
-
-
-#set y_lim for confidence intervals, but very small values
-y_lim = c(min(-log((cs+2*sqrt(csse))[-(1:2)])),max(-log((cs-2*sqrt(csse))[-(1:2)])))
-
-plot(log10(1:n),-log(cs),type="l")# ylim=y_lim
-lines(log10(3:n),-log((cs+2*sqrt(csse))[-(1:2)]),type="l",col=2)#
-lines(log10(3:n),-log((cs-2*sqrt(csse))[-(1:2)]),type="l",col=2)#
-lines(log10(c(1,n)),fit1$objective*c(1,1),col=3)
-
-### Problem C
+k <- 1000
+L <- nll.LA.ImportWeight(fit1$par,X,k=k,seed=16345)
+c(-L,logLik(fit.glm.pois), logLik(fit.glm.bin),-fit1$objective)
 
 
 
 
 
-
+#pois_fitted_value = predict(fit.glm.pois,newdata = clo[order(clo$time),], type="response", se.fit=TRUE)
+#plot(clo$time, clo$clo)
+#points(clo$time[order(clo$time)],pois_fitted_value$fit, col=gg_color_hue(2))
 
